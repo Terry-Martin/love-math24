5fd6dbe67c29bd700d05afe91abc4e658f4f03d4
Terry-Martin <terry.martin@gmail.com>
2024-06-10 21:34:55 +0000

    Generate 2 random number

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,10 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 })
 
-
+/**
+ * The main "loop", called when the script is first loaded
+ * and after the users answer has been processed
+ */
 function runGame() {
 
+    // Create two random numbers between 1 and 25
+    let num1 = math.floor(math.random() * 25) + 1;
+    let num2 = math.floor(math.random() * 25) + 1;
+
 }
+
 
 function checkAnswer() {
 
