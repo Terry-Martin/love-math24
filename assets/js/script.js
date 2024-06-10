d17c349d46dceb7ef8970971a2618968ba81cff7
Terry-Martin <terry.martin@gmail.com>
2024-06-10 20:58:16 +0000

    Add empty game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function increamentScore() {
+
+}
+
+function increamentWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivisionQuestion() {
+    
+}
